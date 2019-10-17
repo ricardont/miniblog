3e923c55801731b93b30672f9fd4835d99f49c6c
Ricardo <ricardo@ricardo.com>
2019-10-17 23:29:32 +0000

    redirect to index after post create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,8 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        #format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to action: "index", notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }

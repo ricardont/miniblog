8f585055c14274af371c964b8b0065d2757ed7b0
Ricardo <ricardo@ricardo.com>
2019-10-21 03:43:49 +0000

    users profile capability

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :email, presence: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
   has_many :posts
-  before_create do  	
+  before_create :set_user_name
+  private 
+  def set_user_name   	
   	pot_name = self.email.split("@").first
   	#User.find
   	self.name ="@#{pot_name}"

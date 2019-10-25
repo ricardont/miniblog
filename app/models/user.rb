6307bdd3a7ff7cdc8a627debcd8edb5684d63556
Ricky <rontiverosmarquez@gmail.com>
2019-10-25 00:00:23 -0600

    add bio and name edit capability

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  validates :email, presence: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
+         :recoverable, :rememberable
+  validates :email, presence: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/, on: :create
   has_many :posts
+  validates :bio, length: { maximum: 300 }
+  validates :name, length: { maximum: 30 }
   before_create :set_user_name
   private 
   def set_user_name   	
   	pot_name = self.email.split("@").first
   	#User.find
-  	self.name ="@#{pot_name}"
+  	self.name ="#{pot_name}"
   end
 end
